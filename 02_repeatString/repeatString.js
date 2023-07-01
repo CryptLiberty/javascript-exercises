05b9f8ec38a13987e182637c42ecd22146da21cc
CryptLiberty <130183977+CryptLiberty@users.noreply.github.com>
2023-07-01 13:59:47 -0400

    This commit completes and fixes the 02 test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString = "";
+    let loopNumber = 0;
+    while (loopNumber < num) {
+        if (num < 0) {
+            break;
+        }
+        newString += string;
+        loopNumber++;
+    };
+    if (num < 0) {
+        return "ERROR";
+    }else {
+        return newString;
+    };
 };
 
 // Do not edit below this line
