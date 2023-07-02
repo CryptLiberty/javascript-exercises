7b294f1be1634d2641078a72d9fbe41de7e583f4
CryptLiberty <130183977+CryptLiberty@users.noreply.github.com>
2023-07-02 11:01:52 -0400

    This commit finished 03 and 04 tests.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
 
+const reverseString = function(userString) {
+let newString = "";
+let reverseStringNumber = userString.length - 1;
+for (let i = 0; i < userString.length; i++) {
+    newString = newString + userString[reverseStringNumber];
+    reverseStringNumber--
+}
+return newString
 };
-
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removal) {
+    for (removalSearch of removal) {
+        arrayIndexRemoval = array.indexOf(removalSearch)
+        if (arrayIndexRemoval === -1){
+            continue;
+        } else {
+            array.splice(arrayIndexRemoval, 1);
+        };
+        
+    }
+    return array;
 };
 
 // Do not edit below this line
