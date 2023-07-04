7f17259df10db32ecc8b3a767685a142018e74f5
CryptLiberty <130183977+CryptLiberty@users.noreply.github.com>
2023-07-04 13:52:33 -0400

    This will fix earlier commit

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,5 @@
 const leapYears = function(birthYear) {
-    if (birthYear % 100 == 0 && birthYear % 400 != 0) {
+    if (birthYear % 100 == 0 && birthYear % 400 == 0) {
         return false;
     }
     else if (birthYear % 4 == 0 || birthYear % 400 == 0) {
