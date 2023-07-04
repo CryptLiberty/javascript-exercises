84be54794aa546aecabde05d1af56c0c62c736b2
CryptLiberty <130183977+CryptLiberty@users.noreply.github.com>
2023-07-04 14:38:29 -0400

    This commit finishes test 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempature) {
+  newTemp = ((tempature - 32) * (5 / 9))
+  return Math.round(newTemp * 10) / 10
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempature) {
+  newTemp = (tempature * (9 / 5) + 32)
+  return Math.round(newTemp * 10) / 10
 };
 
 // Do not edit below this line
