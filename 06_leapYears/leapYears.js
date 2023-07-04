90dc473fca41c3ce27c5487859bc2549d3c73d9b
CryptLiberty <130183977+CryptLiberty@users.noreply.github.com>
2023-07-04 13:50:13 -0400

    This commit finishes test 06.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(birthYear) {
+    if (birthYear % 100 == 0 && birthYear % 400 != 0) {
+        return false;
+    }
+    else if (birthYear % 4 == 0 || birthYear % 400 == 0) {
+        return true;
+    } else {return false;};
 };
 
 // Do not edit below this line
