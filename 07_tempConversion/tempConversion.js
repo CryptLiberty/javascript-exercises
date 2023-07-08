e27b4d0637fd87acee1f513f0fa3ade72f798da9
CryptLiberty <130183977+CryptLiberty@users.noreply.github.com>
2023-07-08 14:07:37 -0400

    This commit completes tests 07-08.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,12 @@
 const convertToCelsius = function(tempature) {
-  newTemp = ((tempature - 32) * (5 / 9))
-  return Math.round(newTemp * 10) / 10
+  tempature = ((tempature - 32) * (5 / 9))
+  return Math.round(tempature * 10) / 10
   
 };
 
 const convertToFahrenheit = function(tempature) {
-  newTemp = (tempature * (9 / 5) + 32)
-  return Math.round(newTemp * 10) / 10
+  tempature = (tempature * (9 / 5) + 32)
+  return Math.round(tempature * 10) / 10
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,55 @@
-const add = function() {
-	
+const add = function(int1, int2) {
+  return int1 + int2;
 };
 
-const subtract = function() {
-	
+const subtract = function(int1, int2) {
+	return int1 - int2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let newSum = 0
+  
+    if (array.length < 0 || array === undefined) {
+      return 0
+    } else if (array.length === 1) {
+      return array[0];
+    } else {
+      for (i in array) {
+        newSum += array[i]
+      }
+      return newSum
+    }
 };
 
-const multiply = function() {
-
+const multiply = function(...int) {
+  let newSum = int[0];
+  let addInt = 1
+  for (i = 0; i < int.length - 1; i++) {
+    newSum = newSum * int[addInt]
+    addInt++
+  }
+  return newSum
 };
 
-const power = function() {
-	
+const power = function(int1, int2) {
+	return int1 ** int2;
 };
 
-const factorial = function() {
-	
+const factorial = function(int1) {
+  let sum = int1
+  let downNumber = int1
+  if (int1 === 0 || int1 === 1){
+    return 1;
+  } else {
+    for (let i = 0; i < int1; i++) {
+      if (--downNumber === 0) {
+        break;
+      } else {
+        sum = sum * downNumber;
+      }
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
